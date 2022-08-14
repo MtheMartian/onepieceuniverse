579c74ead73516ee0c55397c5256d095296d4fc9
MtheMartian <marvin.altidor@outlook.com>
2022-08-13 22:39:25 -0400

    One Piece Universe

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+const characterCard = {
+  submitChar: document.querySelector('#addCharBtn'),
+  updateCharCard: document.querySelectorAll('.editCard'),
+  actualUpdateBtn: document.querySelector('#updateCardBtn'),
+  characterId: ""
+}
+
+Array.from(characterCard.updateCharCard).forEach((x, i) =>{
+  characterCard.updateCharCard[i].onclick = getCardId;
+})
+
+characterCard.actualUpdateBtn.addEventListener('click', updateCard);
+
+async function updateCard(){
+  const cardId = characterCard.characterId;
+  const charNameU = document.querySelector('#charNameU').value;
+  const charAgeU = document.querySelector('#charAgeU').value;
+  const charFruitU = document.querySelector('#charFruitU').value;
+  const charHakiU = document.querySelector('#charHakiU').value;
+  const imgURLU = document.querySelector('#imgURLU').value;
+  try{
+    const response = await fetch('/updateCard', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'itemFromJS': cardId,
+        'charNameU': charNameU,
+        'charAgeU': charAgeU,
+        'charFruitU': charFruitU,
+        'charHakiU': charHakiU,
+        'imgURLU': imgURLU
+      })
+    });
+    const data = await response.json();
+    location.reload();
+  }
+  catch(err){
+    console.log(`Didn't work! ${err}`);
+    location.reload();
+  }
+}
+
+function getCardId(event){
+  characterCard.characterId = event.srcElement.id
+  console.log(characterCard.characterId);
+}
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const ejs = require('ejs');
+const cors = require('cors');
+const { response } = require('express');
+const app = express();
+const PORT = 8000;
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'onepieceuniverse'
+
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+  .then(client =>{
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName)
+  })
+
+app.set('view engine', 'ejs');
+app.use(express.static('assets'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (request, response) =>{
+  db.collection('characters').find().toArray()
+  .then(data =>{
+    response.render('index.ejs', {info: data});
+  })
+  .catch(err =>{
+    console.log(`Something went wrong! ${err}`);
+  })
+})
+
+app.post('/addCharacter', (request, response) =>{
+  db.collection('characters').insertOne(
+    {charName: request.body.charName, charAge: request.body.charAge,
+      charFruit: request.body.charFruit, charhaki: request.body.charHaki,
+      imgURL: request.body.imgURL,
+      id: Math.ceil(Math.random()*1000).toString()
+    })
+  .then(result =>{
+    console.log('Character added!');
+    response.redirect('/');
+  })
+  .catch(err =>{
+    console.log(`Was unable to add character! ${err}`);
+  })
+})
+
+app.put('/updateCard', (request, response) =>{
+  db.collection('characters').updateOne({id: request.body.itemFromJS},{
+    $set: {
+        charName: request.body.charNameU,
+        charAge: request.body.charAgeU,
+        charFruit: request.body.charFruitU,
+        charhaki: request.body.charHakiU,
+        imgURL: request.body.imgURLU,
+      }
+    },
+    {
+      upsert: false
+    })
+  .then(result =>{
+    console.log(`Updated!`);
+    response.redirect('/');
+  })
+  .catch(err =>{
+    console.log(`Ruh oh! ${err}`);
+  })
+})
+
+app.listen(process.env.PORT || PORT, () =>{
+  console.log(`Server running on port ${PORT}`);
+})
